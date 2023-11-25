630fe2f80ba1ceff90ec32e14c1d02d1681149bd
Cakebear <144569838+CakebearCreative@users.noreply.github.com>
2023-11-25 21:18:40 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 
+// script.js
+document.addEventListener('DOMContentLoaded', () => {
+    const sections = document.querySelectorAll('section');
+    const navLinks = document.querySelectorAll('nav a');
+
+    navLinks.forEach(link => {
+        link.addEventListener('click', (e) => {
+            e.preventDefault();
+            const targetSection = document.querySelector(e.target.getAttribute('href'));
+            sections.forEach(section => section.style.display = 'none');
+            targetSection.style.display = 'block';
+        });
+    });
+});
