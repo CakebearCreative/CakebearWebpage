658ba302711b4672497810113a2e6ae679a539af
Cakebear <144569838+CakebearCreative@users.noreply.github.com>
2023-11-26 21:16:35 +0100

    Update fetchtxt.js

--- fetchtxt.js
+++ fetchtxt.js
@@ -1,4 +1,4 @@
-function loadContent(fileName, targetId) {
+function loadContent(fileName, targetId, clickedElement) {
     // Path to the directory containing the .txt files
     const baseDir = 'prompts/';
 
@@ -10,6 +10,20 @@ function loadContent(fileName, targetId) {
         .then(response => response.text())
         .then(data => {
             document.getElementById(targetId).textContent = data;
+            // Update the active prompt styling
+            updateActivePromptStyle(clickedElement);
         })
         .catch(error => console.error('Error loading content:', error));
+}
+
+
+function updateActivePromptStyle(clickedElement) {
+    const allPrompts = document.querySelectorAll('.sidebar a'); // Adjust the selector as needed
+    allPrompts.forEach(prompt => {
+        prompt.classList.remove('active-prompt');
+    });
+
+    // Add 'active-prompt' class to the clicked prompt
+    // Assuming 'this' refers to the clicked anchor element
+    clickedElement.classList.add('active-prompt');
 }
